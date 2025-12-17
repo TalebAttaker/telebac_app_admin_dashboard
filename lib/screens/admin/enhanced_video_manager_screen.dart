ee58f14f715281f84fb8a50be629d4b851f104ae
Taleb Ahmed <taharou7@gmail.com>
2025-12-17 19:37:13 +0000

    fix: Add missing foundation import for kIsWeb

--- lib/screens/admin/enhanced_video_manager_screen.dart
+++ lib/screens/admin/enhanced_video_manager_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
